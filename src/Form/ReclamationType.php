d9cd4b65e63d0e48dbfae122d67c6f4436c6ef40
alislm20 <ali.dagdoug@esprit.tn>
2023-04-04 14:42:34 +0200

    rec

--- src/Form/ReclamationType.php
+++ src/Form/ReclamationType.php
@@ -7,12 +7,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ReclamationType extends AbstractType
+class Reclamation3Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ;
+            ->add('objet',TextType::class)
+            ->add('contenu',TextType::class)
+            ->add('date')
+            ->add('statut',TextType::class)
+            ->add('idvehicule',EntityType::class,
+                    ['class'=>Vehicule::class,
+                    'choice_label'=>'idvehicule'])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
