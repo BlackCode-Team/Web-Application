7426eb7e1b77e59a709f1fd872319c357fbfd481
Rania-Ben-Abdallah <rania.benabdallah@etudiant-isi.utm.tn>
2023-04-05 02:30:26 +0100

    v5

---
+++ var/cache/dev/twig/d7/d74a5933fea952da3368aa42f2e3c46b6b035fc1d6957b2fd6254ba3e5740534.php
@@ -0,0 +1,302 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* vehicule/index.html.twig */
+class __TwigTemplate_7ba919568f6a4842ce294938c8c70d6f31c5f54004f80eafba9552fd47fc0905 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "back.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "vehicule/index.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "vehicule/index.html.twig"));
+
+        $this->parent = $this->loadTemplate("back.html.twig", "vehicule/index.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Vehicules ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 5
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 6
+        echo "    <div class=\"row\" id=\"cancel-row\">
+        <div class=\"col-xl-12 col-lg-12 col-sm-12  layout-spacing\">
+            <div class=\"statbox widget box box-shadow\">
+                <div class=\"widget-header\">
+                    <div class=\"row\">
+                        <div class=\"col-xl-12 col-md-12 col-sm-12 col-12\">
+                            <h4>Liste des vehicules</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class=\"widget-content widget-content-area\">
+                    <div class=\"table-responsive mb-4\">
+                        <table id=\"zero-config\" class=\"table table-striped table-hover table-bordered\" style=\"width:100%\">
+                            <thead>
+                                <tr>
+                                    <th>Modele</th>
+                                    <th>Matricule</th>
+                                    <th>Puissance</th>
+                                    <th>Type</th>
+                                    <th>Prix</th>
+                                    <th>Batterie</th>
+                                    <th>Image</th>
+                                    <th>Status</th>
+                                    <th>actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            ";
+        // line 33
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["vehicules"]) || array_key_exists("vehicules", $context) ? $context["vehicules"] : (function () { throw new RuntimeError('Variable "vehicules" does not exist.', 33, $this->source); })()));
+        $context['_iterated'] = false;
+        foreach ($context['_seq'] as $context["_key"] => $context["vehicule"]) {
+            // line 34
+            echo "                                <tr>
+                                    <td>";
+            // line 35
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["vehicule"], "modele", [], "any", false, false, false, 35), "html", null, true);
+            echo "</td>
+                                    <td>";
+            // line 36
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["vehicule"], "matricule", [], "any", false, false, false, 36), "html", null, true);
+            echo "</td>
+                                    <td>";
+            // line 37
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["vehicule"], "puissance", [], "any", false, false, false, 37), "html", null, true);
+            echo "</td>
+                                    <td>";
+            // line 38
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["vehicule"], "type", [], "any", false, false, false, 38), "html", null, true);
+            echo "</td>
+                                    <td>";
+            // line 39
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["vehicule"], "prix", [], "any", false, false, false, 39), "html", null, true);
+            echo "</td>
+                                    <td>";
+            // line 40
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["vehicule"], "batterie", [], "any", false, false, false, 40), "html", null, true);
+            echo "</td>
+                                    <td><img src=\"";
+            // line 41
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl(("assets/" . twig_get_attribute($this->env, $this->source, $context["vehicule"], "image", [], "any", false, false, false, 41))), "html", null, true);
+            echo "\" /></td>
+                                    <td>";
+            // line 42
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["vehicule"], "status", [], "any", false, false, false, 42), "html", null, true);
+            echo "</td>
+                                    <td>
+                                        <a href=\"";
+            // line 44
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_vehicule_show", ["idvehicule" => twig_get_attribute($this->env, $this->source, $context["vehicule"], "idvehicule", [], "any", false, false, false, 44)]), "html", null, true);
+            echo "\">show</a>
+                                        <a href=\"";
+            // line 45
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_vehicule_edit", ["idvehicule" => twig_get_attribute($this->env, $this->source, $context["vehicule"], "idvehicule", [], "any", false, false, false, 45)]), "html", null, true);
+            echo "\">edit</a>
+                                    </td>
+                                </tr>
+                            ";
+            $context['_iterated'] = true;
+        }
+        if (!$context['_iterated']) {
+            // line 49
+            echo "                                <tr>
+                                    <td colspan=\"10\">no records found</td>
+                                </tr>
+                            ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['vehicule'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 53
+        echo "                            </tbody>
+                        </table>
+                    </div>
+                    <div class=\"pagination-section\">
+                        <ul class=\"pagination pagination-style-1 pagination-rounded justify-content-end mt-3 mb-3\">
+                            <li><a href=\"javascript:void(0);\">«</a></li>
+                            <li><a href=\"javascript:void(0);\">1</a></li>
+                            <li><a href=\"javascript:void(0);\">2</a></li>
+                            <li><a href=\"javascript:void(0);\">3</a></li>
+                            <li><a href=\"javascript:void(0);\">4</a></li>
+                            <li><a href=\"javascript:void(0);\">5</a></li>
+                            <li><a href=\"javascript:void(0);\">»</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href=\"";
+        // line 71
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_vehicule_new");
+        echo "\">Create new</a>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "vehicule/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  200 => 71,  180 => 53,  171 => 49,  162 => 45,  158 => 44,  153 => 42,  149 => 41,  145 => 40,  141 => 39,  137 => 38,  133 => 37,  129 => 36,  125 => 35,  122 => 34,  117 => 33,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'back.html.twig' %}
+
+{% block title %}Vehicules {% endblock %}
+
+{% block body %}
+    <div class=\"row\" id=\"cancel-row\">
+        <div class=\"col-xl-12 col-lg-12 col-sm-12  layout-spacing\">
+            <div class=\"statbox widget box box-shadow\">
+                <div class=\"widget-header\">
+                    <div class=\"row\">
+                        <div class=\"col-xl-12 col-md-12 col-sm-12 col-12\">
+                            <h4>Liste des vehicules</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class=\"widget-content widget-content-area\">
+                    <div class=\"table-responsive mb-4\">
+                        <table id=\"zero-config\" class=\"table table-striped table-hover table-bordered\" style=\"width:100%\">
+                            <thead>
+                                <tr>
+                                    <th>Modele</th>
+                                    <th>Matricule</th>
+                                    <th>Puissance</th>
+                                    <th>Type</th>
+                                    <th>Prix</th>
+                                    <th>Batterie</th>
+                                    <th>Image</th>
+                                    <th>Status</th>
+                                    <th>actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {% for vehicule in vehicules %}
+                                <tr>
+                                    <td>{{ vehicule.modele }}</td>
+                                    <td>{{ vehicule.matricule }}</td>
+                                    <td>{{ vehicule.puissance }}</td>
+                                    <td>{{ vehicule.type }}</td>
+                                    <td>{{ vehicule.prix }}</td>
+                                    <td>{{ vehicule.batterie }}</td>
+                                    <td><img src=\"{{asset('assets/'~vehicule.image)}}\" /></td>
+                                    <td>{{ vehicule.status }}</td>
+                                    <td>
+                                        <a href=\"{{ path('app_vehicule_show', {'idvehicule': vehicule.idvehicule}) }}\">show</a>
+                                        <a href=\"{{ path('app_vehicule_edit', {'idvehicule': vehicule.idvehicule}) }}\">edit</a>
+                                    </td>
+                                </tr>
+                            {% else %}
+                                <tr>
+                                    <td colspan=\"10\">no records found</td>
+                                </tr>
+                            {% endfor %}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class=\"pagination-section\">
+                        <ul class=\"pagination pagination-style-1 pagination-rounded justify-content-end mt-3 mb-3\">
+                            <li><a href=\"javascript:void(0);\">«</a></li>
+                            <li><a href=\"javascript:void(0);\">1</a></li>
+                            <li><a href=\"javascript:void(0);\">2</a></li>
+                            <li><a href=\"javascript:void(0);\">3</a></li>
+                            <li><a href=\"javascript:void(0);\">4</a></li>
+                            <li><a href=\"javascript:void(0);\">5</a></li>
+                            <li><a href=\"javascript:void(0);\">»</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href=\"{{ path('app_vehicule_new') }}\">Create new</a>
+{% endblock %}
+", "vehicule/index.html.twig", "C:\\Users\\Rania2\\Documents\\Web-Application\\templates\\vehicule\\index.html.twig");
+    }
+}
